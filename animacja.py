c8a95e975daa1b753c3726b24aae484ffc752739
ping010 <ping0.wenta@gmail.com>
2023-09-28 21:14:32 +0200

    Animacja
    Dodałem animację, usunąłem stary model slowliva, ustawiłem w twojej funkcji bordery dla animacji

---
+++ animacja.py
@@ -0,0 +1,86 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.LEFT_KEY, self.RIGHT_KEY, self.UP_KEY, self.DOWN_KEY, self.FACING_LEFT = False, False, False, False, False
+        self.load_frames()
+        self.rect = self.idle_frames_left[0].get_rect()
+        self.rect.midbottom = (665,350)
+        self.current_frame = 0
+        self.last_updated = 0
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.state = 'idle'
+        self.current_image = self.idle_frames_left[0]
+
+    def draw(self, display):
+        display.blit(self.current_image, self.rect)
+
+    def update(self):
+        self.velocity_x, self.velocity_y = 0, 0
+        if self.LEFT_KEY:
+            self.velocity_x = -5
+            self.rect.x += self.velocity_x
+        if self.RIGHT_KEY:
+            self.velocity_x = 5
+            self.rect.x += self.velocity_x
+        if self.UP_KEY:
+            self.velocity_y = -5
+            self.rect.y += self.velocity_y
+        if self.DOWN_KEY:
+            self.velocity_y = 5
+            self.rect.y += self.velocity_y
+        self.set_state()
+        self.animate()
+
+    def set_state(self):
+        self.state = 'idle'
+        if self.velocity_x > 0 or self.velocity_y < 0:
+            self.state = 'moving right'
+        elif self.velocity_x < 0 or self.velocity_y > 0:
+            self.state = 'moving left'
+
+    def animate(self):
+        now = pygame.time.get_ticks()
+        if self.state == 'idle':
+            if now - self.last_updated > 200:
+                self.last_updated = now
+                self.current_frame = (self.current_frame + 1) % len(self.idle_frames_left)
+                if self.FACING_LEFT:
+                    self.current_image = self.idle_frames_right[self.current_frame]
+                elif not self.FACING_LEFT:
+                    self.current_image = self.idle_frames_left[self.current_frame]
+        else:
+            if now - self.last_updated > 100:
+                self.last_updated = now
+                self.current_frame = (self.current_frame + 1) % len(self.walking_frames_left)
+                if self.state == 'moving left':
+                    self.current_image = self.walking_frames_right[self.current_frame]
+                elif self.state == 'moving right':
+                    self.current_image = self.walking_frames_left[self.current_frame]
+
+
+    def load_frames(self):
+        self.idle_frames_left = [pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_0_delay-0.1s.gif').convert(), (125,110)),
+                                 pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_1_delay-0.1s.gif').convert(), (125,110)),
+                                 pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_2_delay-0.1s.gif').convert(), (125,110)),
+                                 pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_3_delay-0.1s.gif').convert(), (125,110)),
+                                 pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_4_delay-0.1s.gif').convert(), (125,110)),
+                                 pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_5_delay-0.1s.gif').convert(), (125,110)),
+                                 pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_6_delay-0.1s.gif').convert(), (125,110)),
+                                 pygame.transform.scale(pygame.image.load('SiemaSlowLife/dino_idle/frame_7_delay-0.1s.gif').convert(), (125,110))]
+        self.walking_frames_left = [pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_0.png').convert(), (125,110)),
+                                    pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_1.png').convert(), (125,110)),
+                                    pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_2.png').convert(), (125,110)),
+                                    pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_3.png').convert(), (125,110)),
+                                    pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_4.png').convert(), (125,110)),
+                                    pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_5.png').convert(), (125,110)),
+                                    pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_6.png').convert(), (125,110)),
+                                    pygame.transform.scale(pygame.image.load('SiemaSlowLife\dino_walk\dino_7.png').convert(), (125,110))]
+        self.idle_frames_right = []
+        for frame in self.idle_frames_left:
+            self.idle_frames_right.append(pygame.transform.flip(frame, True, False))
+        self.walking_frames_right = []
+        for frame in self.walking_frames_left:
+            self.walking_frames_right.append(pygame.transform.flip(frame, True, False))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import os
+from animacja import Player
 
+pygame.init()
 width = 1280
 height = 720
 screen = pygame.display.set_mode((width,height))
@@ -12,47 +14,64 @@ slow_life = pygame.transform.scale(slow_life_image,(slow_life_width,slow_life_he
 mapa = pygame.transform.scale(pygame.image.load(os.path.join("SiemaSlowLife","mapa.jpg")),(width,height))
 vel = 5
 FPS = 60
+slowcik = Player()
 
 
-def draw_windows(slow):
+def draw_windows():
     screen.blit(mapa,(0,0))
-    screen.blit(slow_life,(slow.x,slow.y))
+    # screen.blit(slow_life,(slow.x,slow.y))
+    slowcik.update()
+    slowcik.draw(screen)
     pygame.display.update()
 
-
-def dino_movement(keys_pressed,slow):
-    def bordery():
-        if slow.x <= 430:
-            slow.x = 430
-
-
-    if keys_pressed[pygame.K_a] and slow.x > 0:
-        slow.x -= vel
-        bordery()
-    if keys_pressed[pygame.K_d] and slow.x + vel + slow.width < 1280:
-        slow.x += vel
-    if keys_pressed[pygame.K_w] and slow.y - vel > 0:
-        slow.y -= vel
-        bordery()
-    if keys_pressed[pygame.K_s] and slow.y - vel + slow.height   < 720:
-        slow.y +=  vel
-        bordery()
-
-
+def bordery():
+    if slowcik.rect.x <= 430:
+        slowcik.rect.x = 430
+    if slowcik.rect.x >= width - slow_life_width:
+        slowcik.rect.x = width - slow_life_width
+    if slowcik.rect.y <= 0:
+        slowcik.rect.y = 0
+    if slowcik.rect.y >= height - slow_life_height:
+        slowcik.rect.y = height - slow_life_height
 
 def main():
     clock = pygame.time.Clock()
-    slow = pygame.Rect(665,350,slow_life_width,slow_life_height)
+    # slow = pygame.Rect(665,350,slow_life_width,slow_life_height)
     run = True
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        keys_pressed = pygame.key.get_pressed()
-        dino_movement(keys_pressed,slow)
-        draw_windows(slow)
-        #print(pygame.mouse.get_pos())
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s and event.key == pygame.K_a:
+                    slowcik.DOWN_KEY, slowcik.LEFT_KEY, slowcik.FACING_LEFT = True, True, True
+                if event.key == pygame.K_s and event.key == pygame.K_d:
+                    slowcik.DOWN_KEY, slowcik.RIGHT_KEY, slowcik.FACING_LEFT = True, True, False
+                if event.key == pygame.K_w and event.key == pygame.K_a:
+                    slowcik.UP_KEY, slowcik.LEFT_KEY, slowcik.FACING_LEFT = True, True, False
+                if event.key == pygame.K_w and event.key == pygame.K_d:
+                    slowcik.UP_KEY, slowcik.RIGHT_KEY, slowcik.FACING_LEFT = True, True, False
+                elif event.key == pygame.K_d:
+                    slowcik.RIGHT_KEY, slowcik.FACING_LEFT = True, False
+                elif event.key == pygame.K_a:
+                    slowcik.LEFT_KEY, slowcik.FACING_LEFT = True, True
+                elif event.key == pygame.K_w:
+                    slowcik.UP_KEY, slowcik.FACING_LEFT = True, False
+                elif event.key == pygame.K_s:
+                    slowcik.DOWN_KEY, slowcik.FACING_LEFT = True, True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    slowcik.RIGHT_KEY = False
+                if event.key == pygame.K_a:
+                    slowcik.LEFT_KEY = False
+                if event.key == pygame.K_w:
+                    slowcik.UP_KEY = False
+                if event.key == pygame.K_s:
+                    slowcik.DOWN_KEY = False
+        bordery()
+        draw_windows()
+        # print(pygame.mouse.get_pos())
     pygame.quit()
 
 if __name__ == "__main__":
