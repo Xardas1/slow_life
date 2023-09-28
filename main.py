76a4ec784623f3c7d9c25590cbcb1ff9ad04c9c8
Maciek <ciufel.maly@gmail.com>
2023-09-28 17:27:32 +0200

    dolny_border

--- main.py
+++ main.py
@@ -21,14 +21,23 @@ def draw_windows(slow):
 
 
 def dino_movement(keys_pressed,slow):
+    def bordery():
+        if slow.x <= 430:
+            slow.x = 430
+
     if keys_pressed[pygame.K_a] and slow.x > 0:
         slow.x -= vel
+        bordery()
     if keys_pressed[pygame.K_d] and slow.x + vel + slow.width < 1280:
         slow.x += vel
     if keys_pressed[pygame.K_w] and slow.y - vel > 0:
         slow.y -= vel
     if keys_pressed[pygame.K_s] and slow.y - vel + slow.height   < 720:
         slow.y +=  vel
+
+def position_of_cursor():
+    
+
 
 def main():
     clock = pygame.time.Clock()
